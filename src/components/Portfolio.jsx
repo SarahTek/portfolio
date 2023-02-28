5c16dd12d1ddf42aa5139aa729ef8a2ea040afeb
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2023-02-28 13:35:00 -0500

    started portfolio section

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import whereTo from '../assets/Portfolio/whereTo.png'
+import mentorConnect from '../assets/Portfolio/mentorConnect.png'
+
+
+const Portfolio = () => {
+  return (
+    <div>Portfolio</div>
+  )
+}
+
+export default Portfolio
