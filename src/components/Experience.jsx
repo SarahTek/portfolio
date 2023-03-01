5cba7d11c6060a42dd59d942217b0008a376821e
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2023-03-01 14:56:15 -0500

    added pictures and style to portfolio section

---
+++ src/components/Experience.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import javascript from '../assets/javascript.png';
+import html from '../assets/html.png';
+import css from '../assets/css.png';
+import reactImage from '../assets/react.png';
+import tailwind from '../assets/tailwind.png';
+import node from '../assets/node.png';
+import github from '../assets/github.png';
+
+
+const Experience = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Experience</p>
+          <p>Technologies I have worked with</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Experience
